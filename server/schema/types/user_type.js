8b3b8cbac56d64e989f75f7eb3a5e2b3791a5674
Robinson <nosnibor1989@gmail.com>
2018-09-14 11:21:01 -0300

    Import client

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -3,12 +3,14 @@ const graphql = require('graphql');
 const {
     GraphQLObjectType,
     GraphQLString,
+    GraphQLID,
 } = graphql;
 
 const UserType = new GraphQLObjectType({
     name: 'UserType',
     fields: {
-        email: { type: GraphQLString }
+        id: { type: GraphQLID },
+        email: { type: GraphQLString },
     },
 });
 
